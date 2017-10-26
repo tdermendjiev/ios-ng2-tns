b6212caf00cbe4ce0df50f7cf4bbd3e2a857a677
Teodor Dermendzhiev <Teodor.Dermendzhiev@progress.com>
2017-10-26 11:49:26 +0300

    Initial Commit

---
+++ MyExistingApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyExistingApp
+//
+//  Created by Teodor Dermendzhiev on 26/10/2017.
+//  Copyright © 2017 Nativescript. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyExistingApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyExistingApp
+//
+//  Created by Teodor Dermendzhiev on 26/10/2017.
+//  Copyright © 2017 Nativescript. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
